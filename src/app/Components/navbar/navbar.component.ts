fa572e905f40f9953c0a64fb40625a09514138ac
Abhishek-90 <holaniabhishek90@gmail.com>
2022-02-22 19:18:41 +0530

    Search Input Close Animation Implemented

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -6,13 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  searchVisible:boolean = false;
+  searchVisible:boolean = false
+  onlySearchDivClassName:string = "search-section"
   constructor() { }
 
   ngOnInit(): void {
   }
 
   showSearch() {
-    this.searchVisible = !this.searchVisible;    
+    this.searchVisible = !this.searchVisible;
+    
+    if(!this.searchVisible) {
+      this.onlySearchDivClassName = "search-section-animation"
+    }
+    
   }
 }
